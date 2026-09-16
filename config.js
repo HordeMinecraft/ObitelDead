cb086c219795a96aac0e2ae8610b86079d174e32
HordeMinecarft <deniswww127@gmail.com>
2026-09-16 22:48:49 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Публичный адрес API. После первого деплоя Cloudflare Worker замени только эту строку.
 // Пример: https://obitel-dead-api.<твой-subdomain>.workers.dev/api/
-const CLOUD_API='https://PASTE-YOUR-WORKER-URL-HERE.workers.dev/api/';
+const CLOUD_API='https://obiteldead.deniswww127.workers.dev/';
 
 const isRemoteFrontend=
  location.hostname==='hordeminecraft.github.io'||
